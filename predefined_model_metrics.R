09cdaa5272c56113845c9915ddd4353d318b9b31
gabrielgomes-f <64420524+gabrielgomes-f@users.noreply.github.com>
2021-11-30 17:07:57 -0300

    Update predefined_model_metrics.R

--- predefined_model_metrics.R
+++ predefined_model_metrics.R
@@ -21,6 +21,9 @@ logistic.model.metrics <- function(formula, data.train, data.test, treshold = 0.
   # Calculanting performance metrics
   logistic.performance.metrics <- summary.measures(logistic.confusion.matrix$table, print = FALSE)
   
+  # Calculating AUC
+  logistic.auc <- rocit(logistic.pred.probability, data.test$Y)$AUC
+  
   # Calculating Chi-Square Statistics
   logistic.chi.square.statistic <- sum(((logistic.pred.probability - data.test$p)^2)/(logistic.pred.probability))
   
@@ -30,13 +33,13 @@ logistic.model.metrics <- function(formula, data.train, data.test, treshold = 0.
   # Calculating run time
   logistic.run.time <- Sys.time() - start
   
-  logistic.output <- data.frame(logistic.performance.metrics, logistic.chi.square.statistic,
+  logistic.output <- data.frame(logistic.performance.metrics, logistic.auc, logistic.chi.square.statistic,
                                 logistic.ks.statistic, logistic.run.time, mean(logistic.pred.probability))
   
   names(logistic.output) <- c("logistic_ACC","logistic_TPR","logistic_TNR","logistic_CSI", "logistic_SSI",
                               "logistic_FAITH","logistic_PDIF","logistic_GS","logistic_P", "logistic_R",
-                              "logistic_F1","logistic_MCC","logistic_ChiSquare","logistic_KS","logistic_RunTime",
-                              "logistic_ProbMean")
+                              "logistic_F1","logistic_MCC","logistic_AUC","logistic_ChiSquare","logistic_KS",
+                              "logistic_RunTime","logistic_ProbMean")
   return(logistic.output)
 }
 
@@ -73,6 +76,9 @@ randomforest.model.metrics <- function(formula, data.train, data.test, treshold 
   # Calculanting performance metrics
   randomforest.performance.metrics <- summary.measures(randomforest.confusion.matrix$table, print = FALSE)
   
+  # Calculating AUC
+  randomforest.auc <- rocit(randomforest.pred.probability, data.test$Y)$AUC
+  
   # Calculating Chi-Square Statistics
   randomforest.chi.square.statistic <- sum(((randomforest.pred.probability - data.test$p)^2)/(randomforest.pred.probability+0.00001))
   
@@ -82,12 +88,12 @@ randomforest.model.metrics <- function(formula, data.train, data.test, treshold 
   # Calculating run time
   randomforest.run.time <- Sys.time() - start
   
-  randomforest.output <- data.frame(randomforest.performance.metrics, randomforest.chi.square.statistic,
+  randomforest.output <- data.frame(randomforest.performance.metrics, randomforest.auc, randomforest.chi.square.statistic,
                                 randomforest.ks.statistic, randomforest.run.time, mean(randomforest.pred.probability))
   
   names(randomforest.output) <- c("randomforest_ACC","randomforest_TPR","randomforest_TNR","randomforest_CSI", "randomforest_SSI",
                               "randomforest_FAITH","randomforest_PDIF","randomforest_GS","randomforest_P", "randomforest_R",
-                              "randomforest_F1","randomforest_MCC","randomforest_ChiSquare","randomforest_KS","randomforest_RunTime",
-                              "randomforest_ProbMean")
+                              "randomforest_F1","randomforest_MCC", "randomforest_AUC", "randomforest_ChiSquare","randomforest_KS",
+                              "randomforest_RunTime","randomforest_ProbMean")
   return(randomforest.output)
 }
