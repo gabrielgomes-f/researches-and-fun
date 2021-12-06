f22cb8c2531905c7fe54138ad884a92283dc5b35
gabrielgomes-f <64420524+gabrielgomes-f@users.noreply.github.com>
2021-12-06 14:46:30 -0300

    Uploading Masters Degree Codes

---
+++ Masters Dissertation/Functions/create_unbalanced_data.R
@@ -0,0 +1,42 @@
+normalize.data <- function(x){
+  if (length(x) > 1){
+    x.mean = mean(x)
+    x.var = var(x)
+    x.sd = sqrt(x.var)
+    x.normalized = (x - x.mean)/x.sd
+    return(x.normalized)
+  }
+  if (length(x) <= 1){
+    stop("Error. The object must have length greater than 1")
+  }
+}
+
+
+# Only works for 5 independent variables
+create.unbalanced.data <- function( sample.size = 1000, beta.0 = 0.5, betas = c(1, 1.5, -1, -1.5, 0.5), lambda = 1, treshold = 'random'){
+  X1 <- normalize.data(runif(sample.size, -4, 4))
+  X2 <- normalize.data(runif(sample.size, -3, 3))
+  X3 <- normalize.data(runif(sample.size, -2, 2))
+  X4 <- normalize.data(runif(sample.size, 0, 2))
+  X5 <- normalize.data(rnorm(sample.size, 0, 1))
+  simulated.data <- data.frame(X1, X2, X3, X4, X5)
+  
+  simulated.probability <- plogis( beta.0 + as.matrix(simulated.data) %*% betas) ** lambda
+  
+  if (treshold == 'random'){
+    U <- matrix(runif(sample.size), sample.size, 1)
+    Y <- as.factor( ifelse( U < simulated.probability, 1, 0))
+  } else if (is.numeric(treshold) == FALSE || treshold < 0 || treshold > 1 ){
+    stop("Insert a valid treshold value. It must be numeric and between 0 and 1.")
+  } else {
+    Y <- as.factor( ifelse (treshold < simulated.probability, 1,0))
+  }
+  
+  simulated.data <- data.frame(simulated.data, Y, simulated.probability)
+  names(simulated.data)[7] <- "p"
+  
+  return(simulated.data)
+}
+
+
+
